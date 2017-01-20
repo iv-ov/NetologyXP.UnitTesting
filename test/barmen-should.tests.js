ccd0e288cdfade02ed476e6c32f4388c338522f0
Dmitry Pavlov <dmitry.pavlov@smartstepgroup.com>
2017-01-20 11:01:50 +0300

    Test naming examples  - barmen.should.tests

--- test/barmen-should.tests.js
+++ test/barmen-should.tests.js
@@ -3,12 +3,33 @@
 var assert = require('assert');
 var Barmen = require('../src/barmen');
 
-//<ClassName>Should.<Act><Assert>
+//
+// <ClassName>Should.<Act>_<Assert>
+//
+// AuthenticationServiceShould
+//   StoreLoggedInUserName_Always
+//
+// LoginPageShould
+//   RedirectToDefaultPage_IfUserLoggedIn
+//
+// DefaultPageShould
+//   ShowSalesReportToAdmin_IfUserIsAdmin
+//
+
 describe('Barmen should', function () {
     describe('pour me 50 grams', function () {
-        it('when I ask 50 grams', function () {
-            let barmen = new Barmen();
-            assert.equal(50, barmen.pour(50));
+        it('if I ask for 50 grams', function () {
+
         })
+
+        it('if I have a promo cocktail coupon', function () {
+
+        });
     })
+
+    describe('call a policeman', function () {
+        it('if I\'m under 18 and ask for a drink', function () {
+
+        });
+    });
 });
