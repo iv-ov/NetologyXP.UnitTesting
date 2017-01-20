458773e7039c8b68d48a5efc21127af446661996
Dmitry Pavlov <dmitry.pavlov@smartstepgroup.com>
2017-01-20 11:09:30 +0300

    Test naming examples  - alcoholic-drinks.js (<Arrange>.<Assert> style)

---
+++ test/alcoholic-drinks.tests.js
@@ -0,0 +1,43 @@
+"use strict";
+
+//
+// <Arrange>.<Assert> - пропущен Action CheckAvailability
+//
+// RegularMovieWith18RateIs
+//   RestrictedForUser
+//   AvailableForAdmin
+//
+// SeriesMovieWith18RateIs
+//   AvailableForChild
+//   AvailableForAdmin
+//
+
+describe('Alcoholic drinks', function () {
+    describe('are available for', function () {
+        it('all visitors older 18', function () {
+
+        });
+
+        it('visitor with a promo code', function () {
+
+        });
+
+        it('regular visitor', function () {
+
+        });
+    });
+
+    describe('are not available for', function () {
+        it('visitor under 18', function () {
+
+        });
+
+        it('drunk visitor', function () {
+
+        });
+
+        it('suspicious visitor', function () {
+
+        });
+    });
+});
